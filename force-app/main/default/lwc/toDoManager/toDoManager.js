0235fb86044abecfc6f820f763ca91ff4d0722e6
harshit6966 <harshit6966@gmail.com>
2022-06-02 11:22:47 +0530

    Dynamic time and greeting getting displayed in SF Org in realtime

--- force-app/main/default/lwc/toDoManager/toDoManager.js
+++ force-app/main/default/lwc/toDoManager/toDoManager.js
@@ -4,35 +4,45 @@ export default class ToDoManager extends LightningElement {
     @track time = "8:12 PM";
     @track greeting = "Good Evening";
 
+    connectedCallback(){
+        this.getTime();
+
+        setInterval(() => {
+            this.getTime();
+        }, 1000 * 60 );
+    }
+    
     getTime(){
         const date = new Date();
         const hour = date.getHours();
         const min = date.getMinutes();
 
-        this.time = `{this.getHour(hour)}:{this.getDoubleDigit(min)}:{this.getMidDay(hour)}`;
+        this.time = `${this.getHour(hour)}:${this.getDoubleDigit(
+            min
+            )} ${this.getMidDay(hour)}`;
         
         this.setGreeting(hour);
     }
 
     getHour(hour){
-        return hour === 0 ? 12 : hour>12 ? (hour-12) : hour; 
+        return hour == 0 ? 12 : hour > 12 ? hour-12 : hour; 
+    }
+
+    getDoubleDigit(digit){
+        return digit < 10 ? "0" + digit : digit;
     }
 
     getMidDay(hour){
         return hour >= 12 ? "PM" : "AM";
     }
 
-    getDoubleDigit(digit){
-        return digit<10 ? "0"+digit : digit;
-    }
-
     setGreeting(hour){
         if(hour < 12){
-            this.greeting("Good Morning"); 
-        } else if(hour >= 12 && hour <= 17){
-            this.greeting("Good Afternoon");
+            this.greeting = "Good Morning"; 
+        } else if(hour >= 12 && hour < 17){
+            this.greeting = "Good Afternoon";
         } else{
-            this.greeting("Good Evening");
+            this.greeting = "Good Evening";
         }
     }
 }
